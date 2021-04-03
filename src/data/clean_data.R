b9bbbfa3e649df8d3f4b817b62bea640967823df
Josh Archer <jearcher@berkeley.edu>
2021-04-03 10:59:33 -0700

    adding script to fix block A ctrl

--- src/data/clean_data.R
+++ src/data/clean_data.R
@@ -1,9 +1,9 @@
 # Use data.table syntax, dplyrs library
 library(data.table)
-library(dplyr)
 library(tidyverse)
 # Ensure the directory is "./src/data"
-data <- fread("../../data/raw/raw_survey_data_main.csv")
+# Read in interim data (fixed mislabeled data)
+data <- fread("../../data/interim/data_block_A_fixed.csv.csv")
 
 # First two columns are not observations 
 # (Why this has to be done twice instead of data[-2,] is beyond me)
@@ -11,6 +11,11 @@ data <- data[-1,]
 data <- data[-1,]
 
 
-# Write to interim until finished
 
-fwrite(data, file = "../../data/interim/interim_data.csv")
+# Write to processed
+
+fwrite(data, file = "../../data/processed/processed_data.csv")
+
+
+
+
