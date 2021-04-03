2f6e1574d4f8e95ca78248cf06c17df21f0787ed
Josh Archer <jearcher@berkeley.edu>
2021-04-03 11:13:39 -0700

    rmv unnecessary rows from interim->processed

--- src/data/clean_data.R
+++ src/data/clean_data.R
@@ -3,15 +3,16 @@ library(data.table)
 library(tidyverse)
 # Ensure the directory is "./src/data"
 # Read in interim data (fixed mislabeled data)
-data <- fread("../../data/interim/data_block_A_fixed.csv.csv")
+data <- fread("../../data/interim/data_block_A_fixed.csv")
 
 # First two columns are not observations 
 # (Why this has to be done twice instead of data[-2,] is beyond me)
 data <- data[-1,]
 data <- data[-1,]
 
-
-
+# Subset only the columns we need (to save space)
+cols2keep <- colnames(data)[c(9, 19:364)]
+data <- subset(data, select =  cols2keep)
 # Write to processed
 
 fwrite(data, file = "../../data/processed/processed_data.csv")
